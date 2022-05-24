d508b5edae7859e72ac8089f4566b132951401cd
Miguel Quezada <mig.aquezada@gmail.com>
2022-05-23 22:21:55 -0400

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Miguel Quezada on 23-05-22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
